7936330ee226eb6f63a6cae3d10640d3c3b7e067
Shourya742 <sshourya17@gmail.com>
2023-10-04 23:54:30 +0530

    Added secondary contrainer

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -5,9 +5,21 @@ import { useSelector } from "react-redux";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    <div>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-    </div>
+    movies.nowPlayingMovies &&
+    movies.popularMovies && (
+      <div className="bg-black">
+        <div className="pl-12 -mt-52 relative z-10">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.popularMovies} />
+          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
   );
 };
 

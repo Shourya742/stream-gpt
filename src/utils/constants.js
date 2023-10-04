451829c8c5d6e889835d4cb60ef4735368533682
Shourya742 <sshourya17@gmail.com>
2023-10-04 18:15:52 +0530

    Configured TMDB and added movies Slice

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,6 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer",
+    Authorization: `Bearer ` + import.meta.env.VITE_API_READ_ACCESS_TOKEN,
   },
 };
